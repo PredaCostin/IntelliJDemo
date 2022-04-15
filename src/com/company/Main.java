96e6e9b95dbed3f0cc1250bec8913bfaf57854b5
ZoomLess <preda_costin@ymail.com>
2022-04-16 02:16:58 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add("bmw");
+        list.add("mercedes");
+        list.add("audi");
+        list.add("ford");
+
+        for(String e : list){
+            System.out.println(e);
+        }
+    }
+}
